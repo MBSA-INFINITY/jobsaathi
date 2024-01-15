fed2bb37c0f33f76782a8813468b1a4b98c432e8
MBSA-INFINITY <mbsaiaditya@gmail.com>
2024-01-15 20:53:11 +0530

    changed

--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
-from langchain.llms import OpenAI
-from langchain import PromptTemplate, LLMChain
-# from langchain.chains import LLMChain
+from langchain_openai import OpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 from db import resume_details_collection
 import os
 import json
