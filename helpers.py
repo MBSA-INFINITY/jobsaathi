19dc5672f95a0105d8e091a7f2a13396d3b8c6de
MBSA-INFINITY <mbsaiaditya@gmail.com>
2024-03-19 20:49:10 +0530

    mbsa

--- helpers.py
+++ helpers.py
@@ -1,8 +1,9 @@
 import re
 from langchain.llms import OpenAI
-from langchain import LLMChain, PromptTemplate
-# from langchain.chains import LLMChain
-# from langchain.prompts import PromptTemplate
+# from langchain_community.llms import OpenAI
+# from langchain import LLMChain, PromptTemplate
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 from db import resume_details_collection, onboarding_details_collection
 import os
 import json
